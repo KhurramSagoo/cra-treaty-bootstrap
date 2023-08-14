e064fcc6359f129e0255080112f4d1cc96d28d35
KhurramSagoo <uaf.khurram@gmail.com>
2023-08-14 17:33:11 +0500

    remaining add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-import Carousel from './components/Carousel';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import TopNav from './components/TopNav'
-
+import Carousel from './components/Carousel';
+import Icons from './components/Icons'
 
 function App() {
   return (
     <>
-    <TopNav />
-    <Carousel />
+    {/* <TopNav /> */}
+    {/* <Carousel /> */}
+    <Icons />
 
     </>
   );
